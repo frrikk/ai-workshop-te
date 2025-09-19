981b8a4e4f50310a62850a6e0091f6db460cb5ee
Frikk <85999465+frrikk@users.noreply.github.com>
2025-09-19 10:16:53 +0200

    Update cn.ts

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,15 +1,1 @@
-import { config } from "@/config";
-import { NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await fetch(config.directLineEndpoint, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${config.studioSecret}`,
-    },
-  });
-
-  const data = await res.json();
-
-  return NextResponse.json(data);
-}
